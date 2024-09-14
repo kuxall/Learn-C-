0373a5469d359c01466e533822fd17b174252c7c
kuxall <kuxall0@gmail.com>
2024-09-13 21:21:44 -0400

    Add datatypes

---
+++ datatypes/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int myNum = 5;
+        double myDoubleNum = 5.99D;
+        char myLetter = 'D';
+        bool myBool = true;
+        string myText = "Hello";
+
+        Console.WriteLine("Output is: ", myNum);
+        Console.WriteLine("Output is: ", myDoubleNum);
+        Console.WriteLine("Output is: ", myLetter);
+        Console.WriteLine("Output is: ", myBool);
+        Console.WriteLine("Output is: ", myText);
+    }
+}
