2277eafbea030a3e2bfc22d8cac85074cf6a39a9
kuxall <kuxall0@gmail.com>
2024-09-18 12:41:32 -0400

    Add looping

---
+++ looping/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more 
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("\nFor Loop: \n");
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine(i);
+        }
+
+        Console.WriteLine("\nWhile Loop: \n");
+        int j = 1;
+        while (j <= 10)
+        {
+            Console.WriteLine(j);
+            j++;
+        }
+
+        Console.WriteLine("\nDo While Loop: \n");
+        int k = 2;
+        do
+        {
+            Console.WriteLine(k);
+            k++;
+        } while (k <= 10);
+    }
+}
