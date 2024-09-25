daf2b3967da48e58925bba47688c1b769a24500a
kuxall <kuxall0@gmail.com>
2024-09-25 12:19:44 -0400

    Add Exercise10

---
+++ Exercise10/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Exercise10
+{
+    public static void Main()
+    {
+        int number1, number2, number3;
+        Console.Write("Input the 1st number: ");
+        number1 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Input the 2nd number: ");
+        number2 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Input the 3rd number: ");
+        number3 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("The result is: " + test(number1, number2, number3));
+    }
+    public static int test(int x, int y, int z)
+    {
+        int result = x + y;
+        return result * z;
+    }
+}
