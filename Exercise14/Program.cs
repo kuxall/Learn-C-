bd43bb7b0f7335670118be32553d6f0f484afa96
kuxall <kuxall0@gmail.com>
2024-09-25 12:20:21 -0400

    Add Exercise14

---
+++ Exercise14/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Exercise14
+{
+    public static void Main()
+    {
+        Console.Write("Enter the amount of Celsius: ");
+        double celsius = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Kelvin = {0}", celsius + 273);
+        Console.WriteLine("Fahrenheit = {0}", celsius * 18 / 10 + 32);
+    }
+}
