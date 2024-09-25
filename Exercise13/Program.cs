c92657dbd5a5afe7dc200fffd97ec6d6a18d1738
kuxall <kuxall0@gmail.com>
2024-09-25 12:20:04 -0400

    Add Exercise 13

---
+++ Exercise13/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Exercise13
+{
+    public static void Main()
+    {
+        int x;
+        Console.Write("Enter a number: ");
+        x = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(x + " + " + x + x + " + " + x + x + x);
+        Console.WriteLine("= " + (x + Convert.ToInt32(string.Format("{0}{0}", x)) + Convert.ToInt32(string.Format("{0}{0}{0}", x))));
+    }
+}
