077bd212ea6441d3ab7045fada688e804e31bd6c
kuxall <kuxall0@gmail.com>
2024-09-25 13:01:15 -0400

    Add vowel consonant program

---
+++ vowelconsonant/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+public class Exercise16
+{
+    static void Main(string[] args)
+    {
+        char ch;
+        Console.Write("\n\n");
+        Console.Write("Accept a character from user and check whether it is a vowel or consonant:\n");
+        Console.Write("------------------------------------------------------------------------");
+        Console.Write("\n\n");
+        Console.Write("Input an alphabet (A-Z or a-z) : ");
+        ch = Convert.ToChar(Console.ReadLine().ToLower());
+        if (ch >= 48 && ch <= 57)
+        {
+            Console.WriteLine("You entered a number, Please enter an alphabet.");
+        }
+        else
+        {
+            switch (ch)
+            {
+                case 'a':
+                    Console.WriteLine("The entered alphabet is a vowel");
+                    break;
+                case 'e':
+                    Console.WriteLine("The entered alphabet is a vowel");
+                    break;
+                case 'i':
+                    Console.WriteLine("The entered alphabet is a vowel");
+                    break;
+                case 'o':
+                    Console.WriteLine("The entered alphabet is a vowel");
+                    break;
+                case 'u':
+                    Console.WriteLine("The entered alphabet is a vowel");
+                    break;
+                default:
+                    Console.WriteLine("The entered alphabet is a consonant");
+                    break;
+            }
+        }
+        Console.ReadKey();
+    }
+}
+ 
