491a24e985529bd8d53bef25a913a67c55d4ed7f
kuxall <kuxall0@gmail.com>
2024-09-25 12:20:35 -0400

    Add Exercise19

---
+++ Exercise19/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+public class Exercise19
+{
+    public static void Main(string[] args )
+    {
+        Console.WriteLine(SumTriple(2, 2));
+        Console.WriteLine(SumTriple(12, 10));
+        Console.WriteLine(SumTriple(-5, 2));
+    }
+    public static int SumTriple(int a, int b)
+    {
+        return a == b ? (a + b) * 3 : a + b;
+        // if a equals b, return the triplesum of a and b, otherwise return the sum
+    }
+}
